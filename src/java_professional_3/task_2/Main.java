81437932b217c304acdba589387b5c811441bb23
Anatoliy Morozov <anatoly.marozaw@gmail.com>
2021-11-05 18:23:06 +0300

    task_3

--- src/java_professional_3/task_2/Main.java
+++ src/java_professional_3/task_2/Main.java
@@ -4,8 +4,6 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("file.txt");
-        file.createNewFile();
         BufferedWriter bw = new BufferedWriter(new FileWriter("file.txt"));
         bw.write("Hello Java!!!");
         bw.close();
